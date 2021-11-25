655f63117201dd63bab3c716caea50ad2946a8d3
Areson <rektf12@gmail.com>
2021-11-25 22:29:41 +0400

    added candles

--- check.py
+++ check.py
@@ -1,13 +1,29 @@
-# AR example
-from statsmodels.tsa.ar_model import AutoReg
+# MA example
+from statsmodels.tsa.arima.model import ARIMA
 from random import random
+import matplotlib.pyplot as plt
+import copy
+
 # contrived dataset
 data = [x + random() for x in range(1, 100)]
+data1 = copy.deepcopy(data)
+data2 = []
 # fit model
-for i in range(20):
-    model = AutoReg(data, lags=1)
+for i in range(10):
+    model = ARIMA(data, order=(1, 1, 1))
     model_fit = model.fit()
     # make prediction
     yhat = model_fit.predict(len(data), len(data))
-    print(yhat)
     data.append(yhat.tolist()[0])
+    data2.append(yhat.tolist()[0])
+    print(yhat)
+
+x1, x2 = [], []
+for i in range(len(data1)):
+    x1.append(i)
+for i in range(len(data2)):
+    x2.append(len(data1)+i-1)
+
+plt.plot(x1, data1)
+plt.plot(x2, data2, color="red")
+plt.show()
--- main.py
+++ main.py
@@ -1,65 +1,105 @@
-import numpy as np
 from scipy import interpolate
-from sklearn import linear_model
-import pandas as pd
 from statsmodels.tsa.ar_model import AutoReg
 import matplotlib.pyplot as plt
+from dataclasses import dataclass
+import pandas as pd
+import numpy as np
+import datetime
+import random
+from math import inf
 
-def linear_extrapolation(prices, cnt):
+
+def generate_candles(last_can, prices, times):
+    candles, i, resolution = [], 0, last_can[0].resolution
+    min_open, max_open = inf, 0
+    min_high, max_high = inf, 0
+    min_low, max_low = inf, 0
+    for candle in last_can:
+        if candle.open < min_open: min_open = candle.open
+        if candle.high < min_high: min_high = candle.high
+        if candle.low < min_low: min_low = candle.low
+
+        if candle.open > max_open: max_open = candle.open
+        if candle.high > max_high: max_high = candle.high
+        if candle.low > max_low: max_low = candle.low
+
+    for price in prices:
+        open = random.uniform(min_open, max_open)
+        high = random.uniform(min_high, max_high)
+        low = random.uniform(min_low, max_low)
+        candle = Candle(open, price, high, low, times[i], resolution)
+        candles.append(candle)
+        i += 1
+
+    return candles
+
+
+def linear_extrapolation(candles, times):
+    cnt = len(times)
+    prices = [candle.close for candle in candles]
+    k = len(prices)
+
     l = len(prices)
     kol_of_prediction = cnt
-    x = pd.DataFrame(np.arange(0, l, 1))
+    x = pd.DataFrame(np.arange(0, k, 1))
     y = pd.DataFrame(np.array(prices))
 
     kol_of_rand = l
     x1 = np.arange(kol_of_rand, kol_of_rand + kol_of_prediction, 1)
 
-    # x1 = [l - 1]
-    # y1 = [y[-1]]
-
-    # lm = linear_model.LinearRegression()
-    # model = lm.fit(x, y)
-    #
-    # predictions = lm.predict(x)
-    #
-    # f = interpolate.interp1d(x, y, fill_value="extrapolate")
-    # for i in range(kol_of_prediction):
-    #     y1.append(f(l + i))
-    #     x1.append(l + i)
-
     extrapolator = interpolate.UnivariateSpline(x, y, k=1)
     y1 = extrapolator(x1)
-    return y1.tolist()
+    res = generate_candles(candles, y1.tolist(), times)
+    return res
 
 
-def linear_regression(prices, cnt):
-    data=[]
-    for i in range(cnt):
-        model = AutoReg(prices, lags=1)
-        model_fit = model.fit()
-        yhat = model_fit.predict(len(prices), len(prices))
-        prices.append(yhat.tolist()[0])
-        data.append(yhat.tolist()[0])
-    return data
+# Autoregression (AR)
+def linear_regression(candles, times):
+    cnt = len(times)
+    prices = [candle.close for candle in candles]
+    k = len(prices)
+
+    model = AutoReg(prices, lags=1)
+    model_fit = model.fit()
+    yhat = model_fit.predict(k, k+cnt-1)
+    res = generate_candles(candles, yhat.tolist(), times)
+    return res
+
+@dataclass
+class Candle:
+    open: float
+    close: float
+    high: float
+    low: float
+    date: datetime
+    resolution: str
 
 
 if __name__ == "__main__":
-    prices, x, i, cnt = [], [], 0, 900
-    file = open('prices.txt', 'r')
-    for line in file:
-        prices.append(float(line))
-        x.append(i)
-        i+=1
+    candles, times = [], []
+    file_candles = open('candles.txt', 'r')
+    x, y = [], []
+    for line in file_candles:
+        l = line[:-1].split(' ')
+        a = datetime.datetime.fromtimestamp(float(l[4]))
+        candle = Candle(float(l[0]), float(l[1]), float(l[2]), float(l[3]), a, l[5])
+        candles.append(candle)
+        x.append(float(l[4]))
+        y.append(float(l[1]))
+
+    x1 = []
+    file_times = open('times.txt', 'r')
+    for line in file_times:
+        times.append(datetime.datetime.fromtimestamp(float(line)))
+        x1.append(float(line))
 
     fig = plt.figure()
-    plt.plot(x, prices, color="blue")
-    x.clear()
-    while i <(len(prices)+cnt):
-        x.append(i)
-        i+=1
+    # plt.plot(x, y, color="blue")
 
-    y1 = linear_extrapolation(prices, cnt)
-    plt.plot(x, y1, color="red")
-    y2 = linear_regression(prices, cnt)
-    # plt.plot(x, y2, color="green")
+    y1 = linear_extrapolation(candles, times)
+    # plt.plot(x1, y1, color="red")
+
+    y2 = linear_regression(candles, times)
+    # plt.plot(x1, y2, color="red")
+
     # plt.show()
